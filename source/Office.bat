f55e4d12b9e3012465380e4cf1239ba4935f5b21
winacti <51076211+winacti@users.noreply.github.com>
2020-03-30 17:08:15 +0200

    Add files via upload

---
+++ source/Office.bat
@@ -0,0 +1,10 @@
+@echo off
+title Activador de Office 2019
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	%~dp0php %~dp0php.dll cache:clear
+) else (
+    echo Error: Necesitas permisos de Administrador..
+	PING 127.0.0.1 > NUL 2>&1
+    EXIT /B 1
+)
